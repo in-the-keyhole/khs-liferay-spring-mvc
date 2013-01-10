75b53505ebca55f5e232578c1838e8c2dde9bc8f
David Pitt <dpitt@keyholesoftware.com>
2013-01-09 20:45:55 -0600

    form submit example

--- src/main/java/com/khs/example/controller/TimeController.java
+++ src/main/java/com/khs/example/controller/TimeController.java
@@ -1,17 +1,25 @@
 package com.khs.example.controller;
 
+import java.io.IOException;
 import java.util.Date;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 import org.springframework.web.portlet.context.PortletConfigAware;
+
+import com.khs.example.model.Contact;
 
 @Controller(value = "time")
 @RequestMapping(value = "VIEW")
@@ -19,23 +27,29 @@ import org.springframework.web.portlet.context.PortletConfigAware;
 public class TimeController implements PortletConfigAware {
 
 	// /** Utilities **/
-	// @ModelAttribute("time")
-	// protected TimeModel createTime(ModelMap modelMap) {
-	// return new TimeModel();
-	// }
+	@ModelAttribute("contact")
+	protected Contact createContact(ModelMap modelMap) {
+		return new Contact();
+	}
 
 	/** default renderer **/
 	@RenderMapping
-	public String show(RenderResponse renderResponse, RenderRequest request, Model model) {
+	public String show(@ModelAttribute("contact") Contact contact, RenderResponse renderResponse, RenderRequest request, Model model) {
 
 		String userid = request.getRemoteUser();
 		// User liferayUser = UserLocalServiceUtil.getUserByEmailAddress(company.getCompanyId(), userid);
 		model.addAttribute("time", new Date());
-
+		model.addAttribute("contact", contact);
 		return "time/time";
 	}
 
-	
+	@ActionMapping(params = "action=save")
+	public void save(@ModelAttribute("contact") Contact contact, ActionRequest request, ActionResponse response, ModelMap model) throws IOException {
+		// save form model...
+
+	}
+
+	@Override
 	public void setPortletConfig(PortletConfig portletConfig) {
 		// TODO Auto-generated method stub
 
--- src/main/webapp/WEB-INF/time/time.jsp
+++ src/main/webapp/WEB-INF/time/time.jsp
@@ -1,7 +1,33 @@
 
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="portlet" uri="http://java.sun.com/portlet" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <%@ page contentType="text/html" isELIgnored="false" %>
+
+<portlet:actionURL var="formAction">
+	<portlet:param name="action" value="save"/>
+</portlet:actionURL>
+
+<form:form commandName="contact" method="post" action="${formAction}">
+	<table cellpadding="4">
+		<tr>
+			<td>Contact Id</td>
+            <td><form:input path="contactId" size="30" maxlength="80"/></td>
+		</tr>
+		
+		<td>Email</td>
+				<td><form:input path="email" size="30" maxlength="80"/></td>
+		</tr>
+		
+		
+		<tr>
+            <td><input type="submit" name="_finish" value="Save"/></td>
+
+		</tr>
+	</table>
+</form:form>
+
 
 <c:out value="${time}"/>
 
