300e619d415a6cd9833b21b8649769e324a15c42
David Pitt <dpitt@keyholesoftware.com>
2013-01-10 14:24:19 -0600

    init

---
+++ src/main/java/com/khs/example/model/Contact.java
@@ -0,0 +1,51 @@
+package com.khs.example.model;
+
+public class Contact {
+
+	private String contactId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+
+	public String getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(String contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
